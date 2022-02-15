c81ce923063bc13be82ad06326d2b01c8759dc1d
Koen van Gilst <koen@koenvangilst.nl>
2022-02-15 20:44:02 +0100

    fix om appels ook in CodeSandbox te zien

--- oefeningen/props-oplossing/src/App.js
+++ oefeningen/props-oplossing/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="container">
       <div className="basket">
-        <Fruit type="apple" />;
+        <Fruit type="apple" />
       </div>
     </div>
   );
