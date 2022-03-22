bbc642b166c351e84c48355ef00cf3ac289984d5
Koen van Gilst <koen@koenvangilst.nl>
2022-03-22 22:16:20 +0100

    Revert "comment out commit hash"
    This reverts commit 107c7fc0bead421894699db9e2bf6f07a9a3b4bf.

--- web/next.config.js
+++ web/next.config.js
@@ -2,16 +2,16 @@ const withTM = require("next-transpile-modules")(["ui"]);
 
 const pkg = require("./package.json");
 
-// const commitHash = require("child_process")
-//   .execSync('git log --pretty=format:"%h" -n1')
-//   .toString()
-//   .trim();
+const commitHash = require("child_process")
+  .execSync('git log --pretty=format:"%h" -n1')
+  .toString()
+  .trim();
 
 module.exports = withTM({
   swcMinify: true,
   reactStrictMode: true,
   env: {
     APP_VERSION: pkg.version,
-    // COMMIT_HASH: commitHash,
+    COMMIT_HASH: commitHash,
   },
 });
